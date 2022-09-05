9f8eea259e96eaf6d75f92bf3db619140e3aa29d
ZyCromerZ <neetroid97@gmail.com>
2022-09-05 14:57:18 +0700

    Update to https://github.com/gcc-mirror/gcc/commit/9f3599704aa55acc2b4da42f990fa3d3d20a941e
        GCC VERSION: arm-zyc-linux-gnueabi-gcc (ZyC-GCC) 11.3.1 20220905
        GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/9f3599704aa55acc2b4da42f990fa3d3d20a941e
    
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- arm-zyc-linux-gnueabi/include/c++/11.3.1/arm-zyc-linux-gnueabi/bits/c++config.h
+++ arm-zyc-linux-gnueabi/include/c++/11.3.1/arm-zyc-linux-gnueabi/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 11
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20220904
+#define __GLIBCXX__ 20220905
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
--- lib/gcc/arm-zyc-linux-gnueabi/11.3.1/plugin/include/plugin-version.h
+++ lib/gcc/arm-zyc-linux-gnueabi/11.3.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "11.3.1";
-static char datestamp[] = "20220904";
+static char datestamp[] = "20220905";
 static char devphase[] = "";
 static char revision[] = "";
 
