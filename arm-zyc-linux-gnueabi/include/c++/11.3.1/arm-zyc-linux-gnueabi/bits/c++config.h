c7c6766f8734cc59275992258b88a0e1c2553cbb
ZyCromerZ <neetroid97@gmail.com>
2022-10-30 14:23:33 +0700

    Update to https://github.com/gcc-mirror/gcc/commit/aa62b10560d852c97f1005acd7e748c9c8809799
        GCC VERSION: arm-zyc-linux-gnueabi-gcc (ZyC-GCC) 11.3.1 20221030
        GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/aa62b10560d852c97f1005acd7e748c9c8809799
    
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- arm-zyc-linux-gnueabi/include/c++/11.3.1/arm-zyc-linux-gnueabi/bits/c++config.h
+++ arm-zyc-linux-gnueabi/include/c++/11.3.1/arm-zyc-linux-gnueabi/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 11
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20221029
+#define __GLIBCXX__ 20221030
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
