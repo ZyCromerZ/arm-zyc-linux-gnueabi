62e4f4dbd10ec1faa03742c67a46a311b2daab18
ZyCromerZ <neetroid97@gmail.com>
2023-02-19 12:34:14 +0700

    Update to https://github.com/gcc-mirror/gcc/commit/19855ac985c492d775a62ac63754d11df0dcbf26
        GCC VERSION: arm-zyc-linux-gnueabi-gcc (ZyC-GCC) 11.3.1 20230219
        GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/19855ac985c492d775a62ac63754d11df0dcbf26
    
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- lib/gcc/arm-zyc-linux-gnueabi/11.3.1/plugin/include/plugin-version.h
+++ lib/gcc/arm-zyc-linux-gnueabi/11.3.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "11.3.1";
-static char datestamp[] = "20230218";
+static char datestamp[] = "20230219";
 static char devphase[] = "";
 static char revision[] = "";
 
