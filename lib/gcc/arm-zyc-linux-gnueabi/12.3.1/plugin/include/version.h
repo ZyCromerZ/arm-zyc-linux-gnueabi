c7260300c723b5af907e5e2a67c36c9b01b46deb
ZyCromerZ <neetroid97@gmail.com>
2023-05-31 13:12:52 +0700

    Update to https://github.com/gcc-mirror/gcc/commit/6bd66f01b2b629d2af66c3ce3c372f05ba59d11b
        GCC VERSION: arm-zyc-linux-gnueabi-gcc (ZyC-GCC) 12.3.1 20230531
        GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/6bd66f01b2b629d2af66c3ce3c372f05ba59d11b
    
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- lib/gcc/arm-zyc-linux-gnueabi/12.3.1/plugin/include/version.h
+++ lib/gcc/arm-zyc-linux-gnueabi/12.3.1/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "12.3.1 20230530"
+#define version_string "12.3.1 20230531"
 #define pkgversion_string "(ZyC-GCC) "
 
 /* This is the location of the online document giving instructions for
